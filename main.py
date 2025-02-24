45bfcd4e2f087524cff9bb7414869424461bd0da
Rafael Baez <baezrafael03@gmail.com>
2025-02-24 15:11:13 -0700

    Version conflicts are fun

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-from datasets import load_dataset
-df = load_dataset("ai4privacy/pii-masking-200k")
-print("Source")
-print(df['train']['source_text'][0])
-print("Target")
-print(df['train']['target_text'][0])
+# from datasets import load_dataset
+# from nltk.tokenize import word_tokenize, MWETokenizer
+# df = load_dataset("ai4privacy/pii-masking-200k")
+# print("Source")
+# print(df['train']['source_text'][0])
+# print("Target")
+# print(df['train']['target_text'][0])
+import numpy as np
+import tensorflow as tf
+print(tf.config.list_physical_devices())
