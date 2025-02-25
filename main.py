4399217203227ddba5342a318c529cefac789e94
Rafael Baez <baezrafael03@gmail.com>
2025-02-25 12:11:20 -0700

    auto download

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
 import urllib.request
-urllib.request.urlretrieve("https://huggingface.co/datasets/ai4privacy/pii-masking-300k/resolve/main/data/train/1english_openpii_30k.jsonl", "./cache/1english_openpii_30k.jsonl")
+import os
+
+data_url = "https://huggingface.co/datasets/ai4privacy/pii-masking-300k/resolve/main/data/train/1english_openpii_30k.jsonl"
+data_path = "./cache/1english_openpii_30k.jsonl"
+if not (os.path.exists("./cache")): os.mkdir("./cache")
+if not (os.path.exists(data_path)): urllib.request.urlretrieve(data_url, data_path)
